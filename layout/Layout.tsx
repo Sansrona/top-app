9aeafe200278379ce3c039a9f227b32c2f701090
Sansrona <kingJaM00@yandex.ru>
2021-07-30 22:16:16 +0600

    HOC layout

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -1,12 +1,12 @@
 import { ILayout } from "./Layout.props";
 import cn from 'classnames';
 import styles from './Rating.module.css';
-import React from "react";
+import React, { FunctionComponent } from "react";
 import { Header } from "./Header/Header";
 import { Sidebar } from "./Sidebar/Sidebar";
 import { Footer } from "./Footer/Footer";
 
-export const Layout = ({children}:ILayout): JSX.Element=> {
+ const Layout = ({children}:ILayout): JSX.Element=> {
     return (<>
         <Header />
         <div>
@@ -19,3 +19,14 @@ export const Layout = ({children}:ILayout): JSX.Element=> {
         </>
     );
 };
+
+
+export const withLayout = <T extends Record<string, unknown>>(Component:FunctionComponent<T>)=> {
+    return function (props:T):JSX.Element{
+        return (
+            <Layout>
+                <Component {...props} />
+            </Layout>
+        )
+    };
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 import { useState } from 'react';
 import { Button, Htag, P, Rating, Tag } from '../components';
-import { Layout } from '../layout/Layout';
+import { withLayout } from '../layout/Layout';
 import styles from '../styles/Home.module.css';
 
-export default function Home():JSX.Element {
+ function Home():JSX.Element {
   const [rating, setRating] = useState<number>(4);
   return (
-    <Layout>
+    <>
       <Htag tag="h1">Текст</Htag>
       <Button appearance='primary' arrow='right'>Press here</Button>
       <Button appearance='ghost' arrow='down'>Press here2</Button>
@@ -20,6 +20,8 @@ export default function Home():JSX.Element {
       <Tag size='m'color='green'>Button</Tag>
       <Tag size='m'color='gray'>Button</Tag>
       <Rating isEditable rating={rating} setRating={setRating}></Rating>
-    </Layout>
+    </>
   );
 }
+
+export default withLayout(Home);
