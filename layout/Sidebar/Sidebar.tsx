bf8d37d6273d7c2954bde111454ed0e384140c7f
Sansrona <kingJaM00@yandex.ru>
2021-08-15 20:58:26 +0600

    add logo, fix sidebar

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import { Menu } from "../Menu/Menu";
 import { ISidebar } from "./Sidebar.props";
+import Logo from '../logo.svg';
+import cn from 'classnames';
+import styles from './Sidebar.module.css';
 
 
-export const Sidebar = ({ ...props }: ISidebar): JSX.Element => {
-    return <div {...props}>
-        <Menu></Menu>
-    </div>;
+export const Sidebar = ({className}:ISidebar): JSX.Element => {
+    return (
+        <div className={cn(styles.sidebar, className)}>
+            <Logo className={styles.logo}/>
+            search
+            <Menu />
+        </div>
+    );
 };
