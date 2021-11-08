8b1b60ac1111395f6b3d0575e6498c4c01fa4188
Sansrona <kingJaM00@yandex.ru>
2021-11-08 20:02:50 +0600

    done project

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -6,12 +6,12 @@ import Arrow from './Arrow.svg';
 export const Button = ({children,arrow='none',appearance,className, ...props}:IButton): JSX.Element => {
     return (
         <button className={cn(styles.button,className, {
-            [styles.primary]:appearance==='primary',
-            [styles.ghost]:appearance==='ghost',
+            [styles.primary]:appearance =='primary',
+            [styles.ghost]:appearance =='ghost',
         })} {...props}>
             {children}
             {arrow !== 'none' && <span className={cn(styles.arrow,{
-                [styles.down]:arrow ==='down'
+                [styles.down]:arrow =='down'
             })}><Arrow/></span>}
         </button>
     );
