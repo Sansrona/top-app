9549e9d4837b42a2b93851744e1efd56cbbd78f6
Sansrona <kingJaM00@yandex.ru>
2021-11-08 23:34:58 +0600

    done project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import { AppProps } from 'next/dist/next-server/lib/router/router';
+import { AppProps } from 'next/app';
 import '../styles/globals.css';
 import Router from 'next/router';
 import Head from 'next/head';
